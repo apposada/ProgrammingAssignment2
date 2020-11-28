f37c5c857dbddda8ec496d1ab20351654504ac11
Alberto Perez-Posada <ap.posada1@gmail.com>
2020-11-28 14:38:20 +0100

    wrote the makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function creates a list of functions with access to the value in the
+## formal argument; i. e. a numeric matrix. These functions are able to 
+## access and mutate the matrix value as well as its inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ s=NULL
+ setmatrix <- function(y){
+   x <<- y
+   s <<- NULL
+ }
+ getmatrix <- function() x
+ setsolve <- function(y){
+   s <<- y
+ }
+ getsolve <- function() s
+ list(setmatrix = setmatrix, getmatrix = getmatrix, 
+      setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function retrieves the value of the inverse matrix stored in its
+## formal argument, if any, and either prints it out or calculates it
+## using the formal argument's functions. Finally it stores it in the formal
+## argument's values.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solv <- x$getsolve()
+  if (!is.null(solv)){
+    print("getting solve from cache")
+    solv
+  } else {
+    data <- x$getmatrix()
+    solv <- solve(data, ...)
+    x$setsolve(solv)
+    solv
+  }
+  ## Return a matrix that is the inverse of 'x'
 }
